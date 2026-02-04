a8ae6c5389bf6694521123789c66c6ee313ca6f8
Crutcher Dunnavant <crutcher@gmail.com>
2026-02-03 22:01:10 -0800

    Document features and cleanup feature documentation
    - Replace manual feature documentation with `document-features` macro for improved clarity and maintainability.
    - Add `document-features` dependency to project.

--- crates/wordchipper/src/lib.rs
+++ crates/wordchipper/src/lib.rs
@@ -15,66 +15,7 @@
 //!
 //! ## Crate Features
 //!
-//! #### feature: ``default``
-//!
-//! * ``client``
-//! * ``download``
-//!
-//!
-//! The default feature does not enable "training".
-//!
-//! #### feature: ``client``
-//!
-//! * ``ahash``
-//! * ``rayon``
-//! * ``std``
-//!
-//! The default client is focused on loading vocabularies and running
-//! high performance encoders / decoders.
-//!
-//! #### feature: ``download``
-//!
-//! * ``wordchipper-disk-cache``
-//! * ``std``
-//!
-//! The download feature enables downloading vocabularies from the internet.
-//!
-//! #### feature: ``training``
-//!
-//! * ``compact_str``
-//! * ``dary_heap``
-//! * ``std``
-//!
-//! The training feature enables the training code.
-//!
-//! #### feature: ``std`` / ``no_std``
-//!
-//! The "std" feature enables the use of the `std` library;
-//! and the "`no_std`" feature enables deps needed when "std" is not enabled.
-//! (Negative feature deps are not stable yet.)
-//!
-//! Note: I am unsure if this is complete. It is tested CI, but I'm unsure
-//! if I've fully covered it; and I haven't worked out a ``no_std`` deploy test yet.
-//!
-//! #### feature: ``ahash``
-//!
-//! This swaps all HashMap/HashSet implementations for ``ahash``; which is a performance
-//! win on many/(most?) modern CPUs.
-//!
-//! This is done by the ``types::hash_types::CommonHash{*}`` type alias machinery.
-//! See also the ``hashbrown`` dep used by ``no_std``.
-//!
-//! #### feature: ``rayon``
-//!
-//! This enables some parallelism wrappers using the ``rayon`` crate.
-//!
-//! TODO: I intend on providing a ``tokio`` based ``async`` parallelism mechanism
-//! as well, to structure more direct ``regex find > encode span`` pipeline parallelism.
-//!
-//! #### feature: ``tracing``
-//!
-//! This enables a number of ``tracing`` instrumentation points.
-//! This is only useful for timing tracing of the library itself.
+#![doc = document_features::document_features!()]
 //!
 //! ## Loading Pretrained Tokenizers
 //!
