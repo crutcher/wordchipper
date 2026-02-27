e43949e5e99923af9312370ebcd5701f1262c994
Crutcher Dunnavant <crutcher@gmail.com>
2026-02-26 18:27:50 -0800

    reduce training-demo complexity

--- crates/wordchipper-training/examples/training-demo/src/main.rs
+++ crates/wordchipper-training/examples/training-demo/src/main.rs
@@ -1,18 +1,16 @@
-use std::{collections::HashSet, sync::Arc, time::Duration};
+use std::{collections::HashSet, sync::Arc};
 
 use arrow::array::{Array, StringArray};
 use clap::Parser;
-use similar::{ChangeTag, TextDiff};
+use compact_str::CompactString;
 use wordchipper::{
-    TokenizerOptions,
     UnifiedTokenVocab,
     VocabIndex,
-    pretrained::openai::OA_O200K_BASE_PATTERN,
-    support::slices::{inner_slice_view, inner_str_view},
-    vocab::{ByteMapVocab, io::save_base64_span_map_path},
+    pretrained::openai::OA_R50K_BASE_PATTERN,
+    vocab::io::save_base64_span_map_path,
 };
 use wordchipper_data::dataset::DatasetCacheConfig;
-use wordchipper_training::BinaryPairVocabTrainerOptions;
+use wordchipper_training::{BinaryPairVocabTrainer, BinaryPairVocabTrainerOptions};
 
 /// Example encoders trainer.
 #[derive(Parser, Debug)]
@@ -24,48 +22,36 @@ pub struct Args {
 
     /// Shards to load.
     #[arg(long, num_args = 1.., default_values_t = vec![0,1,2,3,4,5,6,7])]
-    pub shards: Vec<usize>,
+    pub nanochat_shards: Vec<usize>,
 
     /// Vocab size.
     #[arg(long, default_value = "199997")]
     pub vocab_size: usize,
 
-    /// Time the avg encode/decode.
-    #[arg(long, default_value = "false")]
-    pub time_encode_decode: bool,
-
     /// Encode/Decode Batch size.
     #[arg(long, default_value = "512")]
     pub batch_size: usize,
 
-    /// Optional Tiktoken save path.
+    /// Regex pattern for tokenizer.
+    #[arg(long, default_value_t = OA_R50K_BASE_PATTERN.as_str().to_string())]
+    pub regex: String,
+
+    /// Vocab save path (.tiktoken format).
     #[arg(long)]
-    pub tiktoken_save_path: Option<String>,
-
-    /// Number of timing batches to use.
-    #[arg(long, default_value = "20")]
-    pub num_timing_batches: usize,
-
-    /// Enable verbose output.
-    #[arg(long, default_value = "false")]
-    pub verbose: bool,
+    pub vocab_path: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    if args.verbose {
-        println!("{:#?}", args);
-    }
+    println!("{:#?}", args);
 
     let cache_config = DatasetCacheConfig::default().with_cache_dir(args.dataset_dir);
-    if args.verbose {
-        println!("{:#?}", cache_config);
-    }
+    println!("{:#?}", cache_config);
 
     let shards: Vec<usize> = {
         let max_shard = cache_config.source.max_shard;
         let mut collected: HashSet<usize> = HashSet::new();
-        for &idx in &args.shards {
+        for &idx in &args.nanochat_shards {
             assert!(idx < max_shard, "shard index out of range");
             collected.insert(idx);
         }
@@ -81,18 +67,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("...");
     cache.load_shards(&shards)?;
 
-    type T = u32;
-    type K = String;
-    type C = u32;
-
     println!();
     println!("Training Tokenizer on shards: {:?}", shards);
     let t0 = std::time::Instant::now();
 
     let vocab_size = args.vocab_size;
-    let options = BinaryPairVocabTrainerOptions::new(OA_O200K_BASE_PATTERN, vocab_size);
+    let options = BinaryPairVocabTrainerOptions::new(args.regex, vocab_size);
 
-    let mut trainer = options.init::<K, C>();
+    let mut trainer: BinaryPairVocabTrainer<CompactString, u32> = options.init();
 
     for &shard in &shards {
         println!("- shard: {}", shard);
@@ -112,11 +94,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let byte_vocab: ByteMapVocab<T> = Default::default();
-
     println!("- train");
-    let vocab: Arc<UnifiedTokenVocab<T>> = trainer
-        .train(byte_vocab.clone())
+    let vocab: Arc<UnifiedTokenVocab<u32>> = trainer
+        .train(Default::default())
         .expect("training failed")
         .into();
 
@@ -124,144 +104,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("- training_duration: {:.2?}", training_duration);
     println!("- vocab_size: {:?}", vocab.max_token());
 
-    if let Some(path) = args.tiktoken_save_path {
-        save_base64_span_map_path(vocab.span_vocab().span_map(), &path)?;
-        println!("- tiktoken vocab: {path:?}");
-    }
-
-    if args.time_encode_decode {
-        let vocab1 = vocab.clone();
-        let tokenizer = TokenizerOptions::default().build(vocab1);
-
-        let mut samples = Vec::new();
-        {
-            for batch in cache
-                .read_cached_batches(shards[0])?
-                .take(args.num_timing_batches)
-            {
-                let batch = batch?;
-                let column = batch
-                    .column_by_name("text")
-                    .expect("failed to find 'text' column in batch")
-                    .as_any()
-                    .downcast_ref::<StringArray>()
-                    .unwrap();
-
-                for val in column {
-                    let val = val.unwrap().to_string();
-                    samples.push(val);
-                }
-            }
-        }
-
-        println!();
-        println!("Samples Summary:");
-        let sample_count = samples.len();
-        println!("- count: {}", sample_count);
-        let total_sample_bytes = samples.iter().map(|s| s.len()).sum::<usize>();
-        let avg_sample_size = total_sample_bytes / sample_count;
-        println!("- avg size: {avg_sample_size}");
-
-        let sample_batches: Vec<&[String]> = samples.chunks(args.batch_size).collect::<Vec<_>>();
-        let num_batches = sample_batches.len();
-
-        println!();
-        println!("Timing Config:");
-        println!("- batch size: {}", args.batch_size);
-
-        println!();
-        println!("Timing Encode:");
-        let mut token_batches: Vec<Vec<Vec<T>>> = Vec::with_capacity(sample_batches.len());
-        let mut total_token_count = 0;
-        let batch_times_ns = sample_batches.iter().map(|batch| {
-            let t0 = std::time::Instant::now();
-            let token_batch: Vec<Vec<T>> = tokenizer
-                .encoder()
-                .try_encode_batch(&inner_str_view(batch))
-                .unwrap();
-            let t1 = std::time::Instant::now();
-
-            total_token_count += token_batch.iter().map(|tokens| tokens.len()).sum::<usize>();
-
-            token_batches.push(token_batch);
-
-            let delay = t1.duration_since(t0);
-            delay.as_nanos() as u64
-        });
-
-        let avg_batch_time_ns = batch_times_ns.sum::<u64>() / num_batches as u64;
-        println!(
-            "- batch avg: {:#?}",
-            Duration::from_nanos(avg_batch_time_ns)
-        );
-
-        let avg_sample_time_ns = avg_batch_time_ns / args.batch_size as u64;
-        println!(
-            "- sample avg: {:#?}",
-            Duration::from_nanos(avg_sample_time_ns)
-        );
-        let b_p_ns = avg_sample_size as f64 / avg_sample_time_ns as f64;
-        let b_p_s = b_p_ns * 1e9;
-        let mb_p_s = b_p_s / 1e6;
-        println!("- avg bps: {:.2} MB/s", mb_p_s);
-
-        println!();
-        println!("Observed Bytes/Token Stats:");
-        println!("- total bytes: {}", total_sample_bytes);
-        println!("- total tokens: {}", total_token_count);
-        println!(
-            "- sample byte/token: {:.2}",
-            total_sample_bytes as f64 / total_token_count as f64
-        );
-
-        println!();
-        let batch_size = args.batch_size;
-        let num_batches1 = token_batches.len();
-        println!("Timing Decode:");
-
-        let batch_times_ns =
-            sample_batches
-                .iter()
-                .zip(token_batches.iter())
-                .map(|(sample, batch)| {
-                    let t0 = std::time::Instant::now();
-                    let decoded_sample = tokenizer
-                        .decoder()
-                        .try_decode_batch_to_strings(&inner_slice_view(batch))
-                        .unwrap()
-                        .unwrap();
-
-                    let t1 = std::time::Instant::now();
-
-                    for (s, d) in sample.iter().zip(decoded_sample.iter()) {
-                        if s != d {
-                            let diff = TextDiff::from_lines(s, d);
-
-                            for change in diff.iter_all_changes() {
-                                let sign = match change.tag() {
-                                    ChangeTag::Delete => "-",
-                                    ChangeTag::Insert => "+",
-                                    ChangeTag::Equal => " ",
-                                };
-                                print!("{}{}", sign, change);
-                            }
-                            panic!("MISMATCH");
-                        }
-                    }
-
-                    let delay = t1.duration_since(t0);
-                    delay.as_nanos() as u64
-                });
-
-        let avg_batch_time_ns = batch_times_ns.sum::<u64>() / num_batches1 as u64;
-        println!("- batch avg: {:?}", Duration::from_nanos(avg_batch_time_ns));
-
-        let avg_sample_time_ns = avg_batch_time_ns / batch_size as u64;
-        println!(
-            "- sample avg: {:?}",
-            Duration::from_nanos(avg_sample_time_ns)
-        );
-    }
+    save_base64_span_map_path(vocab.span_vocab().span_map(), &args.vocab_path)?;
+    println!("- tiktoken vocab: {:?}", args.vocab_path);
 
     Ok(())
 }
