5854ba06f4e4beef05b8d3b7801501768222dcf2
Crutcher Dunnavant <crutcher@gmail.com>
2026-02-27 21:51:40 -0800

    Add `next_span` method with unit tests to lexers (#250)
    Add `next_span` method and unit tests to lexers
    
    - Implemented `next_span` method for `O200kLexer`, `R50kLexer`, and `Cl100kLexer`.
    - Added associated unit tests to validate behavior.

--- crates/wordchipper/src/spanners/span_lexers/logos/o200k.rs
+++ crates/wordchipper/src/spanners/span_lexers/logos/o200k.rs
@@ -91,6 +91,22 @@ inventory::submit! {
 }
 
 impl SpanLexer for O200kLexer {
+    fn next_span(
+        &self,
+        text: &str,
+        offset: usize,
+    ) -> Option<(usize, usize)> {
+        let mut next_span: Option<(usize, usize)> = None;
+        self.for_each_word(text, offset, &mut |span_ref| match span_ref {
+            SpanRef::Word(r) => {
+                next_span = Some((r.start, r.end));
+                false
+            }
+            _ => true,
+        });
+        next_span
+    }
+
     fn for_each_word(
         &self,
         text: &str,
@@ -116,13 +132,29 @@ impl SpanLexer for O200kLexer {
 mod tests {
     use super::*;
     use crate::{
-        alloc::{sync::Arc, vec::Vec},
+        alloc::{sync::Arc, vec, vec::Vec},
         spanners::{TextSpanner, span_lexers::LexerTextSpanner},
     };
 
     /// Build a `TextSpanner` from a logos lexer with no specials.
     fn spanner(lexer: impl SpanLexer + 'static) -> LexerTextSpanner {
         LexerTextSpanner::new(Arc::new(lexer), None)
+    }
+
+    #[test]
+    fn test_span_lexer() {
+        let lexer = O200kLexer {};
+
+        let text = "hello world";
+        let mut spans: Vec<SpanRef> = Default::default();
+        lexer.for_each_word(text, 0, &mut |span_ref| {
+            spans.push(span_ref);
+            true
+        });
+        assert_eq!(spans, vec![SpanRef::Word(0..5), SpanRef::Word(5..11),]);
+
+        assert_eq!(lexer.next_span(text, 0), Some((0, 5)));
+        assert_eq!(lexer.next_span(&text[5..], 5), Some((5, 11)));
     }
 
     #[test]
