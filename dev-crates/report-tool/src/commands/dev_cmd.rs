5006c3ae8ccef824ed04b4ebd1cb974116594986
Crutcher Dunnavant <crutcher@gmail.com>
2026-03-05 11:48:11 -0800

    Tuning work on brandx graphs.

--- dev-crates/report-tool/src/commands/dev_cmd.rs
+++ dev-crates/report-tool/src/commands/dev_cmd.rs
@@ -11,7 +11,7 @@ use plotters::{
         ShapeStyle,
         *,
     },
-    style::full_palette as palette,
+    style::full_palette as colors,
 };
 use wordchipper_cli_util::logging::LogArgs;
 
@@ -50,11 +50,8 @@ impl DevArgs {
         let output_dir = Path::new(&self.output_dir);
         std::fs::create_dir_all(output_dir)?;
 
-        build_external_tgraph(
-            &self.model,
-            &output_dir.join(format!("tgraph.external.{}.svg", self.model)),
-            &data,
-        )?;
+        build_external_tgraph(&self.model, "buffer_sweep", &output_dir, &data)?;
+
         for accel in [false, true] {
             build_internal_tgraph(
                 &self.model,
@@ -89,15 +86,9 @@ struct Point {
     pub value: f64,
 }
 
-enum SeriesKind {
-    Internal,
-    External,
-}
-
 struct Series {
     pub name: String,
     pub style: ShapeStyle,
-    pub kind: SeriesKind,
     pub points: Vec<Point>,
 }
 impl Series {
@@ -128,15 +119,25 @@ impl Series {
 
 fn external_styles() -> BTreeMap<String, ShapeStyle> {
     [
-        ("bpe_openai".to_string(), palette::GREEN_400.filled()),
-        ("tiktoken".to_string(), palette::RED_200.filled()),
-        ("tokenizers".to_string(), palette::BLUE_300.filled()),
+        ("bpe_openai".to_string(), colors::PURPLE_200.filled()),
+        ("tiktoken".to_string(), colors::RED_200.filled()),
+        ("tokenizers".to_string(), colors::LIGHTBLUE_200.filled()),
     ]
     .iter()
     .cloned()
     .collect()
 }
 
+fn fmin(
+    a: f64,
+    b: f64,
+) -> f64 {
+    match a.partial_cmp(&b).unwrap() {
+        std::cmp::Ordering::Less => a,
+        std::cmp::Ordering::Equal => a,
+        std::cmp::Ordering::Greater => b,
+    }
+}
 fn fmax(
     a: f64,
     b: f64,
@@ -148,8 +149,23 @@ fn fmax(
     }
 }
 
-fn bps(br: &BenchResult) -> f64 {
-    br.throughput_bps.as_ref().unwrap().mean.unwrap()
+fn median_bps(br: &BenchResult) -> f64 {
+    br.throughput_bps.as_ref().unwrap().median.unwrap()
+}
+
+fn as_points<F>(
+    obs: &[(u32, BenchResult)],
+    f: F,
+) -> Vec<Point>
+where
+    F: Fn(u32, &BenchResult) -> f64,
+{
+    obs.iter()
+        .map(|(threads, br)| Point {
+            threads: *threads,
+            value: f(*threads, br),
+        })
+        .collect()
 }
 
 fn build_internal_rel_tgraph<P: AsRef<Path>>(
@@ -163,11 +179,11 @@ fn build_internal_rel_tgraph<P: AsRef<Path>>(
     log::info!("Plotting to {}", plot_path.display());
 
     let span_map: BTreeMap<&str, ShapeStyle> = [
-        ("bpe_backtrack", palette::GREY_A400.filled()),
-        ("buffer_sweep", palette::GREEN_A400.filled()),
-        ("merge_heap", palette::BLUE_A400.filled()),
-        ("priority_merge", palette::PINK_A700.filled()),
-        ("tail_sweep", palette::DEEPPURPLE_A400.filled()),
+        ("bpe_backtrack", colors::GREY_A400.filled()),
+        ("buffer_sweep", colors::GREEN_A400.filled()),
+        ("merge_heap", colors::BLUE_A400.filled()),
+        ("priority_merge", colors::PINK_A700.filled()),
+        ("tail_sweep", colors::DEEPPURPLE_A400.filled()),
     ]
     .iter()
     .cloned()
@@ -180,18 +196,6 @@ fn build_internal_rel_tgraph<P: AsRef<Path>>(
         )
     };
 
-    let mut baseline: BTreeMap<u32, f64> = Default::default();
-    for span in span_map.keys() {
-        if let Some(series_data) = data.select_series(&span_key(span)) {
-            for (threads, bench_result) in series_data.iter() {
-                let bps = bps(bench_result);
-
-                let entry = baseline.entry(*threads).or_default();
-                *entry = fmax(*entry, bps);
-            }
-        }
-    }
-
     let mut plot_series: Vec<Series> = Default::default();
     for (span, style) in span_map.iter() {
         if let Some(series_data) = data.select_series(&span_key(span)) {
@@ -200,16 +204,23 @@ fn build_internal_rel_tgraph<P: AsRef<Path>>(
             plot_series.push(Series {
                 name: span.to_string(),
                 style,
-                kind: SeriesKind::Internal,
-                points: series_data
-                    .iter()
-                    .map(|(threads, bench_result)| Point {
-                        threads: *threads,
-                        value: bps(bench_result) / baseline[threads],
-                    })
-                    .collect(),
+                points: as_points(&series_data, |_, br| median_bps(br)),
             })
         }
+    }
+
+    // Normalize the points to the max value.
+    let mut baseline: BTreeMap<u32, f64> = Default::default();
+    for series in plot_series.iter() {
+        for point in series.points.iter() {
+            let entry = baseline.entry(point.threads).or_default();
+            *entry = fmax(*entry, point.value);
+        }
+    }
+    for series in plot_series.iter_mut() {
+        series.points.iter_mut().for_each(|point| {
+            point.value /= baseline[&point.threads];
+        })
     }
 
     let min_threads = plot_series.iter().map(|s| s.min_threads()).min().unwrap();
@@ -226,7 +237,8 @@ fn build_internal_rel_tgraph<P: AsRef<Path>>(
         .unwrap();
 
     let root = SVGBackend::new(plot_path, (640, 480)).into_drawing_area();
-    root.fill(&palette::WHITE)?;
+    root.fill(&colors::WHITE)?;
+
     let mut chart = ChartBuilder::on(&root)
         .caption(
             format!(
@@ -247,7 +259,7 @@ fn build_internal_rel_tgraph<P: AsRef<Path>>(
     chart
         .configure_mesh()
         .x_desc("Thread Count")
-        .y_desc("Relative Throughput")
+        .y_desc("Relative Median Throughput")
         .draw()?;
 
     for pseries in plot_series {
@@ -297,11 +309,11 @@ fn build_internal_tgraph<P: AsRef<Path>>(
     log::info!("Plotting to {}", plot_path.display());
 
     let span_map: BTreeMap<&str, ShapeStyle> = [
-        ("bpe_backtrack", palette::GREY_A400.filled()),
-        ("buffer_sweep", palette::GREEN_A400.filled()),
-        ("merge_heap", palette::BLUE_A400.filled()),
-        ("priority_merge", palette::PINK_A700.filled()),
-        ("tail_sweep", palette::DEEPPURPLE_A400.filled()),
+        ("bpe_backtrack", colors::GREY_A400.filled()),
+        ("buffer_sweep", colors::GREEN_A400.filled()),
+        ("merge_heap", colors::BLUE_A400.filled()),
+        ("priority_merge", colors::PINK_A700.filled()),
+        ("tail_sweep", colors::DEEPPURPLE_A400.filled()),
     ]
     .iter()
     .cloned()
@@ -321,14 +333,7 @@ fn build_internal_tgraph<P: AsRef<Path>>(
             plot_series.push(Series {
                 name: span.to_string(),
                 style,
-                kind: SeriesKind::Internal,
-                points: series_data
-                    .iter()
-                    .map(|(threads, bench_result)| Point {
-                        threads: *threads,
-                        value: bps(bench_result),
-                    })
-                    .collect(),
+                points: as_points(&series_data, |_, br| median_bps(br)),
             })
         }
     }
@@ -347,7 +352,7 @@ fn build_internal_tgraph<P: AsRef<Path>>(
         .unwrap();
 
     let root = SVGBackend::new(plot_path, (640, 480)).into_drawing_area();
-    root.fill(&palette::WHITE)?;
+    root.fill(&colors::WHITE)?;
     let mut chart = ChartBuilder::on(&root)
         .caption(
             format!(
@@ -368,7 +373,7 @@ fn build_internal_tgraph<P: AsRef<Path>>(
     chart
         .configure_mesh()
         .x_desc("Thread Count")
-        .y_desc("Throughput")
+        .y_desc("Median Throughput")
         .y_label_formatter(&|&bps| {
             format!("{}/s", humansize::format_size_i(bps, humansize::BINARY))
         })
@@ -412,14 +417,11 @@ fn build_internal_tgraph<P: AsRef<Path>>(
 }
 fn build_external_tgraph<P: AsRef<Path>>(
     model: &str,
-    plot_path: &P,
+    span_encoder: &str,
+    output_dir: &P,
     data: &ParBenchData,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let plot_path = plot_path.as_ref();
-
-    log::info!("Plotting to {}", plot_path.display());
-
-    let mut plot_series: Vec<Series> = Default::default();
+    let output_dir = output_dir.as_ref();
 
     let series_names = data
         .series_names()
@@ -427,151 +429,177 @@ fn build_external_tgraph<P: AsRef<Path>>(
         .filter(|name| name.contains(model))
         .collect::<Vec<_>>();
 
+    let mut brandx_group: Vec<Series> = Default::default();
     for (ext, style) in external_styles().iter() {
         if let Some(name) = series_names.iter().find(|name| name.contains(ext)) {
             let series_data = data.select_series(name).unwrap();
 
-            plot_series.push(Series {
+            brandx_group.push(Series {
                 name: ext.to_string(),
                 style: *style,
-                kind: SeriesKind::External,
-                points: series_data
-                    .iter()
-                    .map(|(threads, bench_result)| Point {
-                        threads: *threads,
-                        value: bench_result.throughput_bps.as_ref().unwrap().mean.unwrap(),
-                    })
-                    .collect(),
+                points: as_points(&series_data, |_, br| median_bps(br)),
             })
         }
     }
 
-    let span_map: BTreeMap<&str, ShapeStyle> = [("buffer_sweep", palette::CYAN_300.into())]
-        .iter()
-        .cloned()
-        .collect();
+    let regex_series = Series {
+        name: "wordchipper:regex".to_string(),
+        style: colors::GREEN_200.filled(),
+        points: as_points(
+            &data
+                .select_series(&format!(
+                    "encoding_parallel::wordchipper::{span_encoder}::{model}"
+                ))
+                .expect("Failed to select regex series"),
+            |_, br| median_bps(br),
+        ),
+    };
 
-    for (span, style) in span_map.iter() {
-        for accel in [false, true] {
-            let sname = format!(
-                "encoding_parallel::wordchipper::{span}::{model}{}",
-                if accel { "_fast" } else { "" },
-            );
+    let logos_series = Series {
+        name: "wordchipper:logos".to_string(),
+        style: colors::CYAN_200.filled(),
+        points: as_points(
+            &data
+                .select_series(&format!(
+                    "encoding_parallel::wordchipper::{span_encoder}::{model}_fast"
+                ))
+                .expect("Failed to select regex series"),
+            |_, br| median_bps(br),
+        ),
+    };
 
-            if let Some(series_data) = data.select_series(&sname) {
-                let display_name = format!("wordchipper:{}", if accel { "logos" } else { "regex" });
+    let min_threads = brandx_group.iter().map(|s| s.min_threads()).min().unwrap();
+    let max_threads = brandx_group.iter().map(|s| s.max_threads()).max().unwrap();
 
-                let style = if accel { style.filled() } else { *style };
-
-                plot_series.push(Series {
-                    name: display_name,
-                    style,
-                    kind: SeriesKind::Internal,
-                    points: series_data
-                        .iter()
-                        .map(|(threads, bench_result)| Point {
-                            threads: *threads,
-                            value: bench_result.throughput_bps.as_ref().unwrap().mean.unwrap(),
-                        })
-                        .collect(),
-                })
-            }
-        }
-    }
-
-    let min_threads = plot_series.iter().map(|s| s.min_threads()).min().unwrap();
-    let max_threads = plot_series.iter().map(|s| s.max_threads()).max().unwrap();
-    let min_bps = plot_series
-        .iter()
-        .map(|s| s.min_bps())
-        .min_by(|a, b| a.partial_cmp(b).unwrap())
-        .unwrap();
-    let max_bps = plot_series
-        .iter()
-        .map(|s| s.max_bps())
-        .max_by(|a, b| a.partial_cmp(b).unwrap())
-        .unwrap();
-
-    let root = SVGBackend::new(plot_path, (640, 480)).into_drawing_area();
-    root.fill(&palette::WHITE)?;
-    let mut chart = ChartBuilder::on(&root)
-        .caption(
-            format!("library vrs, model: \"{}\"", model),
-            ("sans-serif", 20).into_font(),
-        )
-        .margin(10)
-        .x_label_area_size(40)
-        .y_label_area_size(90)
-        .build_cartesian_2d(
-            (min_threads..max_threads).log_scale().base(2.0),
-            (min_bps..max_bps).log_scale().base(2.0),
-        )?;
-
-    chart
-        .configure_mesh()
-        .x_desc("Thread Count")
-        .y_desc("Throughput")
-        .y_label_formatter(&|&bps| {
-            format!("{}/s", humansize::format_size_i(bps, humansize::BINARY))
-        })
-        .draw()?;
-
-    for pseries in plot_series {
-        let name = &pseries.name;
-        let style = pseries.style;
-        let points: Vec<(u32, f64)> = pseries
-            .points
+    let min_bps = fmin(
+        brandx_group
             .iter()
-            .map(|p| (p.threads, p.value))
-            .collect();
+            .map(|s| s.min_bps())
+            .min_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap(),
+        regex_series.min_bps(),
+    );
+    let max_bps = fmax(
+        brandx_group
+            .iter()
+            .map(|s| s.max_bps())
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap(),
+        regex_series.max_bps(),
+    );
 
-        match pseries.kind {
-            SeriesKind::Internal => {
-                let size = 2;
-                chart
-                    .draw_series(points.iter().map(|coord| {
-                        EmptyElement::at(*coord)
-                            + Circle::new((0, 0), size + 2, BLACK.stroke_width(2))
-                            + Circle::new((0, 0), size, style)
-                    }))?
-                    .label(name)
-                    .legend(move |coord| {
-                        EmptyElement::at(coord)
-                            + Circle::new((0, 0), size + 2, BLACK.stroke_width(2))
-                            + Circle::new((0, 0), size, style)
-                    });
-            }
-            SeriesKind::External => {
-                let size = 4;
-                chart
-                    .draw_series(points.iter().map(|coord| {
-                        EmptyElement::at(*coord)
-                            + TriangleMarker::new((0, 0), size + 2, BLACK.stroke_width(3))
-                            + TriangleMarker::new((0, 0), size, style)
-                    }))?
-                    .label(name)
-                    .legend(move |coord| {
-                        EmptyElement::at(coord)
-                            + TriangleMarker::new((0, 0), size + 2, BLACK.stroke_width(3))
-                            + TriangleMarker::new((0, 0), size, style)
-                    });
-            }
+    for include_logos in [false, true] {
+        let chart_name = if include_logos { "logos" } else { "regex" };
+
+        let plot_path = output_dir.join(format!("wc_{chart_name}_vrs_brandx.rust.{model}.svg"));
+        log::info!("Plotting to {}", plot_path.display());
+
+        let min_bps = if include_logos {
+            fmin(min_bps, logos_series.min_bps())
+        } else {
+            min_bps
+        };
+        let max_bps = if include_logos {
+            fmax(max_bps, logos_series.max_bps())
+        } else {
+            max_bps
+        };
+
+        let root = SVGBackend::new(&plot_path, (640, 480)).into_drawing_area();
+        root.fill(&colors::WHITE)?;
+        let mut chart = ChartBuilder::on(&root)
+            .caption(
+                format!("wordchipper:{chart_name} vrs brandx, rust, model: \"{model}\"",),
+                ("sans-serif", 20).into_font(),
+            )
+            .margin(10)
+            .x_label_area_size(40)
+            .y_label_area_size(90)
+            .build_cartesian_2d(
+                (min_threads..max_threads).log_scale().base(2.0),
+                (min_bps..max_bps).log_scale().base(2.0),
+            )?;
+
+        chart
+            .configure_mesh()
+            .x_desc("Thread Count")
+            .y_desc("Median Throughput")
+            .y_label_formatter(&|&bps| {
+                format!("{}/s", humansize::format_size_i(bps, humansize::BINARY))
+            })
+            .draw()?;
+
+        let size = 6;
+
+        for pseries in &brandx_group {
+            let name = &pseries.name;
+            let style = pseries.style;
+            let points: Vec<(u32, f64)> = pseries
+                .points
+                .iter()
+                .map(|p| (p.threads, p.value))
+                .collect();
+
+            chart.draw_series(LineSeries::new(
+                pseries.points.iter().map(|p| (p.threads, p.value)),
+                style,
+            ))?;
+
+            chart
+                .draw_series(points.iter().map(|coord| {
+                    EmptyElement::at(*coord)
+                        + TriangleMarker::new((0, 0), size + 2, BLACK.stroke_width(1))
+                        + TriangleMarker::new((0, 0), size, style)
+                }))?
+                .label(name)
+                .legend(move |coord| {
+                    EmptyElement::at(coord)
+                        + TriangleMarker::new((0, 0), size + 2, BLACK.stroke_width(1))
+                        + TriangleMarker::new((0, 0), size, style)
+                });
         }
 
-        chart.draw_series(LineSeries::new(
-            pseries.points.iter().map(|p| (p.threads, p.value)),
-            style,
-        ))?;
+        for pseries in if include_logos {
+            vec![&logos_series, &regex_series]
+        } else {
+            vec![&regex_series]
+        } {
+            let name = &pseries.name;
+            let style = pseries.style;
+            let points: Vec<(u32, f64)> = pseries
+                .points
+                .iter()
+                .map(|p| (p.threads, p.value))
+                .collect();
+
+            chart.draw_series(LineSeries::new(
+                pseries.points.iter().map(|p| (p.threads, p.value)),
+                style,
+            ))?;
+
+            chart
+                .draw_series(points.iter().map(|coord| {
+                    EmptyElement::at(*coord)
+                        + Circle::new((0, 0), size + 1, BLACK.filled())
+                        + Circle::new((0, 0), size, style)
+                }))?
+                .label(name)
+                .legend(move |coord| {
+                    EmptyElement::at(coord)
+                        + Circle::new((0, 0), size + 1, BLACK.filled())
+                        + Circle::new((0, 0), size, style)
+                });
+        }
+
+        chart
+            .configure_series_labels()
+            .position(SeriesLabelPosition::LowerRight)
+            .background_style(WHITE.mix(0.8))
+            .border_style(BLACK)
+            .draw()?;
+
+        root.present()?;
     }
-
-    chart
-        .configure_series_labels()
-        .position(SeriesLabelPosition::LowerRight)
-        .background_style(WHITE.mix(0.8))
-        .border_style(BLACK)
-        .draw()?;
-
-    root.present()?;
 
     Ok(())
 }
